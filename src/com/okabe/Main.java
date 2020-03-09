5140b232c7014e3a74e66face654e4c6cddbefc5
daigookabe <daigookabe@gmail.com>
2020-03-09 17:13:15 +0900

    argsではなくscannerでデータの取得を行えるように変更

--- src/com/okabe/Main.java
+++ src/com/okabe/Main.java
@@ -1,31 +1,39 @@
 package com.okabe;
 
-
-import com.okabe.card.Card;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String args[]) {
+        System.out.println("どのガチャを引きますか？　1：メダロットガチャ　2：ミニ四駆ガチャ");
 
-    public static void main(String[] args) {
-        // argsエラーチェック
-        final Gacha gacha = new Gacha();
+        Scanner scan = new Scanner(System.in);
 
-        try {
-            boolean argsCheck = gacha.isError(args);
-            if (argsCheck) {
-                return;
-            }
-        } catch(IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            return;
+        int gachaNumber = scan.nextInt();
+
+        System.out.println("ノーマルチケットは何枚使用しますか？");
+
+        int normalCount = scan.nextInt();
+
+        System.out.println("スペシャルチケットは何枚使用しますか？");
+
+        int specialCount = scan.nextInt();
+
+        System.out.println("---------------------------------");
+        System.out.println("引くガチャ ： " + printData.gachaSelect(gachaNumber));
+        System.out.println("ノーマルチケット ： " + normalCount + "枚");
+        System.out.println("スペシャルチケット ： " + specialCount + "枚");
+        System.out.println("---------------------------------");
+        System.out.println("でよろしいですか？　はい：1　いいえ：2");
+
+        int judge = scan.nextInt();
+
+        if(judge == 2) {
+            System.out.println("処理を中止します");
+            System.exit(0);
+        } else {
+            Gacha.doGacha(normalCount, specialCount);
         }
 
-        int normalCount = Integer.parseInt(args[0]);
-        int specialCount = Integer.parseInt(args[1]);
-
-        gacha.doGacha(normalCount, specialCount);
-
-        final Card card = new Card();
-
-        card.draw();
+        System.out.println("END");
     }
 }
