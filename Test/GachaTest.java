bbbb93841a8fce3976a0a6779ee5ba70b7a233f2
daigookabe <daigookabe@gmail.com>
2020-03-16 10:33:00 +0900

    rarity.cavの取り込み実装

--- Test/GachaTest.java
+++ Test/GachaTest.java
@@ -15,19 +15,43 @@ public class GachaTest {
         System.setOut(new PrintStream(out));
 
         //テスト対象メソッド呼び出し
-        Gacha.doGacha(10, 3);
-
+        try {
+            Gacha.doGacha(10, 3);
+        } catch (Exception e) {
+            System.out.println("doGachaでエラー");
+        }
         String outPutLog = out.toString();
-        assertThat(outPutLog.substring(outPutLog.length()-4), is("END" + System.lineSeparator()));
+        assertThat(outPutLog.substring(outPutLog.length() - 4), is("END" + System.lineSeparator()));
     }
 
     @Test
     public void getNormalGachaItemTest() {
+        String normalName = null;
 
+        try {
+            normalName = Gacha.getNormalGachaItem(50);
+        } catch (Exception e) {
+            System.out.println("getNormalGachaItemでエラー");
+        }
         //テスト対象メソッド呼び出し
-        String normalName = Gacha.getNormalGachaItem(1);
         System.out.println(normalName);
 
         //todo 期待値を書く
     }
+
+    @Test
+    public void getSpecialGachaItemTest() {
+        String normalName = null;
+
+        try {
+            normalName = Gacha.getSpecialGachaItem(50);
+        } catch (Exception e) {
+            System.out.println("getSpecialGachaItemでエラー");
+        }
+        //テスト対象メソッド呼び出し
+        System.out.println(normalName);
+
+        //todo 期待値を書く
+    }
+
 }
